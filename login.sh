074fdacbba5d65088e5878971ad875c4d9388d8f
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-01-18 13:38:50 -0700

    “comment”

--- login.sh
+++ login.sh
@@ -1,3 +1,3 @@
-curl -v -d "@login.json" -X POST https://dev.stedi.me/login
+curl -v -d "@login.json" -X POST -H  "Content-Type:application/json" http://localhost:3000/login
 
 
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require("express")
+const express = require ("express");
 
 const app = express();
 
 const port = 3000
 
+const bodyParser = require ('body-parser')
+
+app.use(bodyParser.json()); //This actually uses body parser class
+
 app.get("/",(req, res) => {
-    res.send("hello adam");
+    res.send("Hello Adam");
+});
+
+app.post('/login', (req,res) =>{
+    const loginUser = req.body.userName;
+    console.log('Login username: '+ loginUser);
+    res.send('Hello '+ loginUser);
 });
 
 app.listen(port, () => {
     console.log("listening");
 });
+
