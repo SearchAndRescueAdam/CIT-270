6f804d4174c33891f357ac08fd3796d2d4365391
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-01-27 14:07:44 -0700

    “comment”

--- server.js
+++ server.js
@@ -14,18 +14,20 @@ const {v4: uuidv4} = require('uuid');
 
 app.use(bodyParser.json()); //This actually uses body parser class
 
+app.use(express.static('public'))
+
 app.get("/",(req, res) => {
     res.send("Hello Adam");
 });
 
-app.post('/login', (req,res) =>{
+app.post('/login', async(req,res) =>{
     const loginUser = req.body.userName;
     const loginPassword = req.body.password;
     console.log('Login username: '+ loginUser);
-    if (loginUser=="yourmom@gmail.com" && loginPassword=="Yourmom4$0"){
+    const correctPassword = await redisClient.hGet('UserMap', loginUser);
+    if (loginPassword==correctPassword){
         const loginToken = uuidv4();
         res.send(loginToken);
-        res.send("Hello")
     } else {
         res.status(401);
         res.send('Inncorrect password for '+ loginUser);
