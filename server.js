d1043292adff3e8cfbd0ab56e2376eff71c505af
SearchAndRescueAdam <80280742+SearchAndRescueAdam@users.noreply.github.com>
2023-03-20 13:30:29 -0600

    added kube

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require ('body-parser')
 
 const Redis = require ('redis');
 
-const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
+const redisClient = Redis.createClient({url:'redis://default:N2vdWGsGX5PTTgS3@redis-stedi-adam:6379'});
 
 const {v4: uuidv4} = require('uuid');
 
